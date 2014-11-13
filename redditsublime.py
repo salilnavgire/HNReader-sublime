6137070687662dc727052a46f8d33e09ff1311f6
salilnavgire <spn245@nyu.edu>
2014-11-13 17:35:44 -0500

    change

--- redditsublime.py
+++ redditsublime.py
@@ -1,9 +1,31 @@
 import sublime, sublime_plugin
+#import urllib2
+#import urllib
 
 class RedditsublimeCommand(sublime_plugin.TextCommand):
+	def validate_urls(self, urls):
+	  messages = []
+	  for url in urls:
+	    try:
+	      request = urllib.Request(url, headers={ "User-Agent" : "Sublime URL Checker" })
+	      response = urllib.urlopen(request, timeout=10)
+	      message = '"%s" is a valid URL.' % url
+	    except Exception:
+	      message = '"%s" is an invalid URL.' % url
+
+	    messages.append(message)
+	    return messages
+
+
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+	  urls = []
+	  selections = self.view.sel()
+	  for selection in selections:
+	    urls.append(self.view.substr(selection))
 
-	
+	  messages = self.validate_urls(urls)
+	  self.show_quick_panel(messages, self.view.window())
 
+	def show_quick_panel(self, messages, window):
+	  window.show_quick_panel(messages, None, sublime.MONOSPACE_FONT)
 
