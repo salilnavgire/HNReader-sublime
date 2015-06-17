52239c1615d672337dbeabe7362d1b6f2c2ec73c
salilnavgire <spn245@nyu.edu>
2015-06-17 18:17:51 -0400

    minor changes

--- hnsublime.py
+++ hnsublime.py
@@ -16,7 +16,7 @@ class hnreaderCommand(sublime_plugin.WindowCommand):
         self.urls = []
         self.texts = []
         self.scores = []
-        for res in top_story_ids[0:10]:
+        for res in top_story_ids[0:30]:
             new_url = base_url+str(res)+'.json?print=pretty'
             # print(new_url)
             sock1 = urllib.request.urlopen(new_url)
@@ -34,7 +34,7 @@ class hnreaderCommand(sublime_plugin.WindowCommand):
     def show_quick_panel1(self, messages, urls, texts, scores, window):
         self.feed_text = []
         for i in range(len(messages)):
-            self.feed_text.append([str(i + 1) + '. ' + str(messages[i]), 'score:'+str(scores[i])+'('+urls[i]+')'])
+            self.feed_text.append([str(i + 1) + '. ' + str(messages[i]), 'score:'+str(scores[i])+'   ('+urls[i]+')'])
 
         window.show_quick_panel(self.feed_text, self.openURL, sublime.MONOSPACE_FONT)
 
