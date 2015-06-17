bb956ea1429f11b0dc4f115594ff9e6373dc7e57
salilnavgire <spn245@nyu.edu>
2015-06-17 15:01:05 -0400

    Working code for getting top tpoics in Hacker news

--- hnsublime.py
+++ hnsublime.py
@@ -1,52 +1,33 @@
-'''
-import sublime, sublime_plugin
-#import urllib2
-#import urllib
-
-class RedditsublimeCommand(sublime_plugin.TextCommand):
-	def validate_urls(self, urls):
-	  messages = []
-	  for url in urls:
-	    try:
-	      request = urllib.Request(url, headers={ "User-Agent" : "Sublime URL Checker" })
-	      response = urllib.urlopen(request, timeout=10)
-	      message = '"%s" is a valid URL.' % url
-	    except Exception:
-	      message = '"%s" is an invalid URL.' % url
-
-	    messages.append(message)
-	    return messages
-
-
-	def run(self, edit):
-	  urls = []
-	  selections = self.view.sel()
-	  for selection in selections:
-	    urls.append(self.view.substr(selection))
-
-	  messages = self.validate_urls(urls)
-	  self.show_quick_panel(messages, self.view.window())
-
-	def show_quick_panel(self, messages, window):
-	  window.show_quick_panel(messages, None, sublime.MONOSPACE_FONT)
-'''
-
 import sublime
 import sublime_plugin
 import urllib
 import json
-url = 'https://news.ycombinator.com'
-sock = urllib.request.urlopen(url)
-print(sock)
-content = sock.read()
-
-print(content)
-
-#sock.close()
-
-#print content
 
 
 class CnpremailerCommand(sublime_plugin.TextCommand):
+    def get_top_news(self):
+        url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+        sock = urllib.request.urlopen(url)
+        top_story_ids = json.loads(sock.read().decode("utf-8", "strict"))
+        base_url = 'https://hacker-news.firebaseio.com/v0/item/'
+
+        titles = []
+        for res in top_story_ids[0:20]:
+            new_url = base_url+str(res)+'.json?print=pretty'
+            # print(new_url)
+            sock1 = urllib.request.urlopen(new_url)
+            strss = json.loads(sock1.read().decode("utf-8", "strict"))
+            titles.append(strss['title'])
+        return titles
+
     def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        urls = []
+        selections = self.view.sel()
+        for selection in selections:
+            urls.append(self.view.substr(selection))
+
+        messages = self.get_top_news()
+        self.show_quick_panel(messages, self.view.window())
+
+    def show_quick_panel(self, messages, window):
+        window.show_quick_panel(messages, None, sublime.MONOSPACE_FONT)
